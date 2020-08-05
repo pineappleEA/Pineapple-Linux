ff176c116fd0a30a11c517be7db5cc59919a034d
mgthepro <17652761+MGThePro@users.noreply.github.com>
2020-08-05 12:07:25 +0200

    fix another minor syntax error

--- pineapple.sh
+++ pineapple.sh
@@ -89,7 +89,7 @@ rm yuzu-windows-msvc-source-*.tar.xz
 #Compilation
 cd $(ls -d yuzu-windows-msvc-source-*)
 find -path ./dist -prune -o -type f -exec sed -i 's/\r$//' {} ';'
-if ! [ -x "$(glxinfo -B | grep "NVIDIA")"] || ! [ -x "$magicnumber" ]; then
+if ! [ -x "$(glxinfo -B | grep "NVIDIA")" ] || ! [ -x "$magicnumber" ]; then
 	printf "Magic Number\n"
 	printf "\033[32;1mNVIDIA\033[0m"
 	printf " only!\n"
